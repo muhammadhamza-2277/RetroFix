dd34a6943598f16a0c001bef0e5b3c18f8f66cbe
muhammadhamza-2277 <muhammadhamza.227710@gmail.com>
2025-08-30 00:46:04 +0500

    vite config file and import is added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";   // ⬅️ Add this line
+import path, { dirname } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
